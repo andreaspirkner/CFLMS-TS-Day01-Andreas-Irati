b8d65eda1842f7df354a82d3089c5ab57ec27dd9
Irati Larreina <irati_6@hotmail.com>
2020-06-17 10:59:01 +0200

    adds basic 1

---
+++ script.js
@@ -0,0 +1,10 @@
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var multiply = function (a, b) { return a * b; };
+for (var _i = 0, numbers_1 = numbers; _i < numbers_1.length; _i++) {
+    var i = numbers_1[_i];
+    for (var _a = 0, numbers_2 = numbers; _a < numbers_2.length; _a++) {
+        var j = numbers_2[_a];
+        var multiplication = (multiply(i, j));
+        document.getElementById("main").innerHTML += "<div> " + j + " x " + i + " = " + multiplication + " </div>";
+    }
+}
---
+++ script.ts
@@ -0,0 +1,10 @@
+const numbers: Array<number> = [1,2,3,4,5,6,7,8,9,10]
+
+let multiply = (a,b) => a*b;
+
+for(let i of numbers){
+	for(let j of numbers){
+		let multiplication = (multiply(i,j))
+		document.getElementById("main").innerHTML += `<div> ${j} x ${i} = ${multiplication} </div>`
+	}
+}
